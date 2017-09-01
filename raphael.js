ffe0ba9b1e5fdffc88eca67df253ea43b93681e9
Tim Heckel <tim@slatebox.com>
2017-09-01 11:45:46 -0500

    Regenned

--- raphael.js
+++ raphael.js
@@ -8,15 +8,8 @@
 // └───────────────────────────────────────────────────────────────────────────────────────────────────────┘ \\
 
 (function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
-	else if(typeof exports === 'object')
-		exports["Raphael"] = factory();
-	else
 		root["Raphael"] = factory();
-})(this, function() {
+})(window, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
